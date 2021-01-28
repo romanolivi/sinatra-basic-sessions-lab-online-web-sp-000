b9dd9fa74593dd115e906af9bde24fa2b5c96d48
Romano Livi <romano_livi@outlook.com>
2021-01-28 19:18:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   post "/checkout" do 
     @sessions = sessions
     item = params[:item]
-    sessions[:item] = item
+    @sessions[:item] = item
   end
   
   configure do 
