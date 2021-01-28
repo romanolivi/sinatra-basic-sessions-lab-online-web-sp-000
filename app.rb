6b7c3acff20aabbb92c26a85ba7bc9c7f6cea693
Romano Livi <romano_livi@outlook.com>
2021-01-28 19:12:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/" do 
+    erb :index 
+  end
+  
+  post "/checkout" do 
+  
+  end
+  
+  configure do 
+    enable :sessions
+  end
 end
