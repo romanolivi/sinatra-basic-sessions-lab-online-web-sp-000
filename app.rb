f3625053bda78148a17fead19519b63acb089776
Romano Livi <romano_livi@outlook.com>
2021-01-28 19:19:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/" do 
+    erb :index 
+  end
+  
+  post "/checkout" do 
+    @sessions = session
+    item = params[:item]
+    @sessions[:item] = item
+  end
+  
+  configure do 
+    enable :sessions
+  end
 end
